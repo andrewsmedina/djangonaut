a5f28e21b9e14d0357260e487425d79adf833fdb
andrews medina <andrewsmedina@gmail.com>
2011-06-01 18:33:22 -0300

    adicionando model de eventos

--- meusite/eventos/models.py
+++ meusite/eventos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+
+   nome = models.CharField(max_length=255)
+   descricao = models.CharField(max_length=255)
+   inicio = models.DateTimeField()
+   final = models.DateTimeField()
