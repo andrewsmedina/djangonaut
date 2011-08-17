7afb941d4d1b88e057749cd0e74cbb179f699196
Andrews Medina <andrewsmedina@gmail.com>
2011-08-17 01:09:47 -0300

    removing trash

--- meusite/eventos/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from eventos.forms import EventoForm
-
-def hello(request):
-    context = {
-        'form': EventoForm()
-    }
-    return render_to_response('hello.html', context)
